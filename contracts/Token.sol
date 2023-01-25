ff84fc821a08a7e43881e6fdddc4fe322ed93cfd
Eslam Shaker <binshakerr@gmail.com>
2023-01-26 00:50:17 +0200

    smoke test

---
+++ contracts/Token.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "hardhat/console.sol";
+
+/**
+ * The Token contract does this and that...
+ */
+contract Token {
+	string public name = 'My Token';
+
+
+  constructor() public {
+    
+  }
+}
