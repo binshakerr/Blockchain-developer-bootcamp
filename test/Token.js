32edfa1d38e4bf00fb16e4987131581ee9243d2a
Eslam Shaker <binshakerr@gmail.com>
2023-01-30 22:10:41 +0200

    token approvals

--- test/Token.js
+++ test/Token.js
@@ -7,7 +7,7 @@ const tokens = (value) => {
 
 describe('Token', () => {
 
-	let token, deployer, reciever
+	let token, deployer, reciever, exchange
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
@@ -15,6 +15,7 @@ describe('Token', () => {
 		let accounts = await ethers.getSigners()
 		deployer = accounts[0]
 		reciever = accounts[1]
+		exchange = accounts[2]
 	})
 
 	describe('Deployment', () => {
@@ -57,6 +58,8 @@ describe('Token', () => {
 
 			it('emits transfer event', async () => {
 				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
 				const args = event.args
 				expect(args.from).to.equal(deployer.address)
 				expect(args.to).to.equal(reciever.address)
@@ -73,6 +76,40 @@ describe('Token', () => {
 
 			it('rejects invalid recepient', async () => {
 				await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+			})
+		})
+	})
+
+	describe('Approving Tokens', () => {
+
+		let amount, transaction, result
+
+		beforeEach(async () => {
+			amount = tokens(100)
+			transaction = await token.connect(deployer).approve(exchange.address, amount)
+			result = await transaction.wait()
+		})
+
+		describe('Success', () => {
+
+			it('allocates an allowance for delegated token spending', async () => {
+				expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+			})
+
+			it('emits approval event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('Approval')
+
+				const args = event.args
+				expect(args.owner).to.equal(deployer.address)
+				expect(args.spender).to.equal(exchange.address)
+				expect(args.value).to.equal(amount)
+			})
+		})
+
+		describe('Failure', () => {
+			it('rejects invalid spender', async () => {
+				await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
 			})
 		})
 	})
